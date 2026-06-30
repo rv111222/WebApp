6576ca470b321515b5f5965c6602b082975025b6
Vijaya Bhaskara Kutala <bhaskarcmd@gmail.com>
2026-06-30 20:41:06 +0530

    added new folder and changes done in index.html related to jquery

---
+++ jquery/index.js
@@ -0,0 +1,6 @@
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>;
+$(document).ready(function () {
+  $("button").click(function () {
+    $("div").animate({ left: "300px" });
+  });
+});
